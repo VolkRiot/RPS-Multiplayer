f878cc518bbd8bd32ae6867449a370f27054bce0
Mikhail Metrikin <metrikin@gmail.com>
2017-03-14 19:04:58 -0700

    Data and back and forth turn logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,110 +1,123 @@
 
+// TODO(DEVELOPER): DELETE ON FINISH
+var debugObj = {};
+
 function RPSgame() {
 
   this.initFirebase();
-
-
-  this.player1 = {
-    name: '',
-    active: false
-  };
-
-  this.player2 = {
-    name: '',
-    active: false
-  };
+  this.player = {};
+  this.playerContainers = ["#player-panel-one", "#player-panel-two"];
+  this.currentTurn = 1;
 
 }
 
 RPSgame.prototype.initFirebase = function () {
 
   this.rpsData = firebase.database();
-
-  this.playersRef = this.rpsData.ref('players');
-  this.player1Ref = this.rpsData.ref('players/player1');
-  this.player2Ref = this.rpsData.ref('players/player2');
-
-  //this.connectedRef = this.rpsData.ref(".info/connected");
+  this.participants = this.rpsData.ref('players');
 
 };
 
+RPSgame.prototype.createButtons = function () {
+
+  $(this.player.container).append($('<button class="btn btn-primary move-buttons" id="rock">').text("Rock"));
+  $(this.player.container).append($('<button class="btn btn-primary move-buttons" id="paper">').text("Paper"));
+  $(this.player.container).append($('<button class="btn btn-primary move-buttons" id="scissors">').text("Scissors"))
+
+};
+
+var RPS = new RPSgame();
 
 $(document).ready(function () {
 
-  var RPS = new RPSgame();
+  $("#player-submit").on('click', function (e) {
 
-  RPS.playersRef.child("players").on('value', function (snapshot) {
-
-    console.log(snapshot.val())
-
-  });
-
-  // Experiment with Player One logic
-
-  $("#player-one-submit").on('click', function (e) {
     e.preventDefault();
 
-    RPS.player1.name = $('#player-one-input').val().trim();
-    RPS.player1.active = true;
+    if($('#player-input').val()){
 
-    if(RPS.player1.name){
-      RPS.player1Ref.set({
-        player: RPS.player1
+      RPS.participants.once('value', function (snapshot) {
+
+        var childNumber = parseInt(snapshot.numChildren());
+
+        if(snapshot.numChildren() <=1){
+          RPS.player.num = childNumber + 1;
+          RPS.player.name = $('#player-input').val().trim();
+          RPS.player.container = RPS.playerContainers[childNumber];
+          RPS.player.playerRef = 'players/' + String(RPS.player.num);
+        }
+
+        RPS.rpsData.ref(RPS.player.playerRef).set({
+          player: RPS.player
+        });
+
+        if(RPS.player.num == 2){
+          RPS.rpsData.ref('turn').set(RPS.currentTurn)
+        }
+
+        $('#player-form').hide();
+
+        RPS.rpsData.ref('turn').on("value", function (snapshot) {
+
+          var turn = snapshot.val();
+
+          if(turn && RPS.player.num == turn){
+            RPS.createButtons();
+          }
+
+        });
+
+        RPS.rpsData.ref('players').child(RPS.player.num).onDisconnect().remove();
+        RPS.rpsData.ref('turn').onDisconnect().remove();
+
       });
-
     }
 
   });
 
-  RPS.player1Ref.on('child_added', function (player) {
+  RPS.participants.on('child_added', function (snapshot) {
 
-      var playerObj = player.val();
+    var players = snapshot.val();
 
-      $("#player-panel-one").empty();
-      $("#player-panel-one").append($('<p class="h4">').text("Welcome " + player.val().name))
+    //TODO: Remove
+    debugObj = players;
 
-
+    $(players.player.container).empty();
+    $(players.player.container).append($('<p class="h4">').text(players.player.name));
+    $(players.player.container).show();
 
   });
 
-  RPS.player1Ref.onDisconnect().remove();
+  $('.player-panel').on('click', '.move-buttons', function () {
 
-  RPS.player1Ref.on('child_removed', function () {
-    $("#player-panel-one").empty().text('Player 1 just left...');
-  });
+    $('.move-buttons').show();
 
+    RPS.player.move = $(this).attr('id');
 
+    RPS.rpsData.ref(RPS.player.playerRef).set({
+      player: RPS.player
+    });
 
-  // Same with player two
-
-  $("#player-two-submit").on('click', function (e) {
-    e.preventDefault();
-
-    RPS.player2.name = $('#player-two-input').val().trim();
-    RPS.player2.active = true;
-
-    if(RPS.player2.name){
-      RPS.player2Ref.set({
-        player: RPS.player2
-      });
-
+    if(RPS.currentTurn == 1){
+      RPS.currentTurn++;
+    }else if(RPS.currentTurn > 1){
+      RPS.currentTurn = 1;
     }
 
-  });
-
-  RPS.player2Ref.on('child_added', function (player) {
-
-
-    var playerObj = player.val();
-
-    $("#player-panel-two").empty();
-    $("#player-panel-two").append($('<p class="h4">').text("Welcome " + player.val().name))
-
+    RPS.rpsData.ref().update({turn : RPS.currentTurn});
+    
+    $('.move-buttons').hide();
 
   });
 
-  RPS.player2Ref.onDisconnect().remove();
+  RPS.rpsData.ref('turn').on('value', function (snapshot) {
+
+    if(snapshot.val()){
+      RPS.currentTurn = snapshot.val();
+    }
+
+  })
 
 
-});
+ }); // End of document.ready()
 
