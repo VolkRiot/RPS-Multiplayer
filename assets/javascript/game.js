bd1f16836f56f89f0742278515c449f41040b852
Mikhail Metrikin <metrikin@gmail.com>
2017-03-15 00:29:32 -0700

    Delicious Spaghetti

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,7 @@ function RPSgame() {
 
   this.initFirebase();
   this.player = {};
+  this.opponent = {};
   this.playerContainers = ["#player-panel-one", "#player-panel-two"];
   this.currentTurn = 1;
 
@@ -26,6 +27,45 @@ RPSgame.prototype.createButtons = function () {
 
 };
 
+RPSgame.prototype.determineWinner = function () {
+
+  var $gameContainer = $('#game-state');
+  var $resultP = $('<p class="h4">');
+
+  $gameContainer.empty();
+
+  if(this.player.move == this.opponent.move){
+
+    $gameContainer.append($resultP.text('You Tied'));
+
+  }else if(this.player.move == "rock" && this.opponent.move == "scissors"){
+    
+    this.player.wins++;
+    this.rpsData.ref(this.player.playerRef + "/player/").update({wins:this.player.wins});
+    $gameContainer.append($resultP.text( this.player.name + ' Won!'));
+
+  }else if(this.player.move == "scissors" && this.opponent.move == "paper"){
+
+    this.player.wins++;
+    this.rpsData.ref(this.player.playerRef + "/player/").update({wins:this.player.wins});
+    $gameContainer.append($resultP.text( this.player.name + ' Won!'));
+
+  }else if(this.player.move == "paper" && this.opponent.move == "rock"){
+
+    this.player.wins++;
+    this.rpsData.ref(this.player.playerRef + "/player/").update({wins:this.player.wins});
+    $gameContainer.append($resultP.text( this.player.name + ' Won!'));
+
+  }else{
+
+    this.player.losses++;
+    this.rpsData.ref(this.player.playerRef + "/player/").update({losses:this.player.losses});
+    $gameContainer.append($resultP.text( this.opponent.name + ' Won!'));
+
+  }
+
+};
+
 var RPS = new RPSgame();
 
 $(document).ready(function () {
@@ -41,10 +81,12 @@ $(document).ready(function () {
         var childNumber = parseInt(snapshot.numChildren());
 
         if(snapshot.numChildren() <=1){
-          RPS.player.num = childNumber + 1;
-          RPS.player.name = $('#player-input').val().trim();
-          RPS.player.container = RPS.playerContainers[childNumber];
-          RPS.player.playerRef = 'players/' + String(RPS.player.num);
+          RPS.player.num = childNumber + 1,
+          RPS.player.name = $('#player-input').val().trim(),
+          RPS.player.container = RPS.playerContainers[childNumber],
+          RPS.player.playerRef = 'players/' + String(RPS.player.num),
+          RPS.player.wins = 0,
+          RPS.player.losses = 0
         }
 
         RPS.rpsData.ref(RPS.player.playerRef).set({
@@ -63,6 +105,13 @@ $(document).ready(function () {
 
           if(turn && RPS.player.num == turn){
             RPS.createButtons();
+          }
+
+          if(turn === 3){
+            RPS.determineWinner();
+            RPS.currentTurn = 1;
+            RPS.rpsData.ref().update({turn : RPS.currentTurn});
+
           }
 
         });
@@ -98,14 +147,10 @@ $(document).ready(function () {
       player: RPS.player
     });
 
-    if(RPS.currentTurn == 1){
-      RPS.currentTurn++;
-    }else if(RPS.currentTurn > 1){
-      RPS.currentTurn = 1;
-    }
+    RPS.currentTurn++;
 
     RPS.rpsData.ref().update({turn : RPS.currentTurn});
-    
+
     $('.move-buttons').hide();
 
   });
@@ -116,8 +161,30 @@ $(document).ready(function () {
       RPS.currentTurn = snapshot.val();
     }
 
-  })
+  });
 
+  RPS.rpsData.ref('players/1/player').on('value', function(snapshot){
+
+    if(snapshot.val()) {
+
+      if (RPS.player.num != 1) {
+        RPS.opponent = snapshot.val();
+      }
+
+    }
+
+  });
+
+  RPS.rpsData.ref('players/2/player').on('value', function (snapshot) {
+
+    if(snapshot.val()) {
+
+      if (RPS.player.num != 2) {
+        RPS.opponent = snapshot.val();
+      }
+
+    }
+  });
 
  }); // End of document.ready()
 
